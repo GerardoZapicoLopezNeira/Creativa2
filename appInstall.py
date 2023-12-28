ce40efcca5b00eab161a8fee2264dbbd9a9501d8
GerardoZapicoLópezNeira <gerardozapico2002@gmail.com>
2023-12-28 20:50:47 +0100

    title changed

--- appInstall.py
+++ appInstall.py
@@ -6,7 +6,7 @@ import sys, subprocess, os
 def pause():
     programPause = input('Press the <ENTER> key to continue...')
 
-def main():
+def installDependencies():
     subprocess.call("sudo apt install python3", shell=True)
     pause()
     subprocess.call("sudo apt install pip3", shell=True)
@@ -16,7 +16,25 @@ def main():
     path = '{}/practica_creativa2/bookinfo/src/productpage'.format(os.getcwd())
     os.chdir(path)
     subprocess.call("pip3 install -r requirements.txt", shell=True)
-    pause()
+    
+
+def modify_title():
+    # Modificar el código de la aplicación para incluir el nombre del grupo
+    grupo_numero = os.getenv("GRUPO_NUMERO", "GrupoX")  # GrupoX es un valor de respaldo si la variable de entorno no está definida
+    productpage_path = "./templates/index.html"
+    
+    with open(productpage_path, "r") as file:
+        content = file.read()
+        content = content.replace("{% block title %}Simple Bookstore App{% endblock %}", "{% block title %}{grupo_numero}{% endblock %}")
+
+    with open(productpage_path, "w") as file:
+        file.write(content)
+
+def startApp():
     subprocess.call('python3 productpage_monolith.py 9080', shell=True)
 
-main()
+installDependencies()
+modify_title()
+pause()
+startApp()
+
