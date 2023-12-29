689e5e0436c646ea3d82959dc464be974b8306f0
GerardoZapicoLópezNeira <gerardozapico2002@gmail.com>
2023-12-29 21:21:46 +0100

    added doc in comments 2.1 py

--- 1/appInstall.py
+++ 1/appInstall.py
@@ -5,26 +5,32 @@ import subprocess
 import os
 import re
 
-def pause() -> None:
+def pause():
+    """Pausa el programa hasta que se presiona la tecla <ENTER>."""
     programPause = input('Press the <ENTER> key to continue...')
 
-def installDependencies() -> None:
+def installDependencies():
+    """Instala las dependencias necesarias para el proyecto."""
     subprocess.call("sudo apt install python3", shell=True)
     pause()
     subprocess.call("sudo apt install pip3", shell=True)
     subprocess.call("sudo apt install git", shell=True)
     subprocess.call("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git", shell=True)
     pause()
+    # Cambia al directorio del proyecto clonado
     path = '{}/practica_creativa2/bookinfo/src/productpage'.format(os.getcwd())
     os.chdir(path)
     subprocess.call("pip3 install -r requirements.txt", shell=True)
+    # Establece la variable de entorno GRUPO_NUMERO
     os.environ["GRUPO_NUMERO"] = "Grupo 13"
 
-def modify_title() -> None:
+def modify_title():
+    """Modifica el código de la aplicación para incluir el nombre del grupo."""
     grupo_numero = os.getenv("GRUPO_NUMERO", "GrupoX")
     productpage_path = "./templates/productpage.html"
     index_path = "./templates/index.html"
     
+    # Modifica el contenido del archivo productpage.html
     with open(productpage_path, "r") as file:
         content = file.read()
         content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}' + grupo_numero + '{% endblock %}', content, flags=re.DOTALL)
@@ -32,6 +38,7 @@ def modify_title() -> None:
     with open(productpage_path, "w") as file:
         file.write(content)
 
+    # Modifica el contenido del archivo index.html
     with open(index_path, "r") as file:
         content = file.read()
         content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}' + grupo_numero + '{% endblock %}', content, flags=re.DOTALL)
@@ -39,15 +46,15 @@ def modify_title() -> None:
     with open(index_path, "w") as file:
         file.write(content)
 
-def startApp() -> None:
+def startApp():
+    """Inicia la aplicación."""
     subprocess.call('python3 productpage_monolith.py 5000', shell=True)
 
+# Instala dependencias, modifica el título y luego inicia la aplicación
 installDependencies()
 modify_title()
 pause()
 startApp()
 
-if __name__ == "__main__":
-    pass
 
 
