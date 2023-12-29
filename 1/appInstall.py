2f8f756450423b8a7a5e6f9f27e51f5c7b4748e0
GerardoZapicoLópezNeira <gerardozapico2002@gmail.com>
2023-12-29 20:47:10 +0100

    pydoc type hints

--- 1/appInstall.py
+++ 1/appInstall.py
@@ -1,12 +1,14 @@
-#!usr/bin/env python 
+#!/usr/bin/env python
 
-import sys, subprocess, os, re # Librerias
+import sys
+import subprocess
+import os
+import re
 
-
-def pause(): # Función para pausar el programa
+def pause() -> None:
     programPause = input('Press the <ENTER> key to continue...')
 
-def installDependencies(): # Función para instalar las dependencias
+def installDependencies() -> None:
     subprocess.call("sudo apt install python3", shell=True)
     pause()
     subprocess.call("sudo apt install pip3", shell=True)
@@ -16,35 +18,36 @@ def installDependencies(): # Función para instalar las dependencias
     path = '{}/practica_creativa2/bookinfo/src/productpage'.format(os.getcwd())
     os.chdir(path)
     subprocess.call("pip3 install -r requirements.txt", shell=True)
-    os.environ["GRUPO_NUMERO"] = "Grupo 13" 
+    os.environ["GRUPO_NUMERO"] = "Grupo 13"
 
-def modify_title(): # Modificar el código de la aplicación para incluir el nombre del grupo
-    grupo_numero = os.getenv("GRUPO_NUMERO", "GrupoX")  # GrupoX es un valor de respaldo si la variable de entorno no está definida
+def modify_title() -> None:
+    grupo_numero = os.getenv("GRUPO_NUMERO", "GrupoX")
     productpage_path = "./templates/productpage.html"
     index_path = "./templates/index.html"
     
     with open(productpage_path, "r") as file:
         content = file.read()
-        content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}'+grupo_numero+'{% endblock %}', content, flags=re.DOTALL)
-
+        content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}' + grupo_numero + '{% endblock %}', content, flags=re.DOTALL)
 
     with open(productpage_path, "w") as file:
         file.write(content)
 
     with open(index_path, "r") as file:
         content = file.read()
-        content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}'+grupo_numero+'{% endblock %}', content, flags=re.DOTALL)
+        content = re.sub(r'{% block title %}(.*?){% endblock %}', '{% block title %}' + grupo_numero + '{% endblock %}', content, flags=re.DOTALL)
 
     with open(index_path, "w") as file:
         file.write(content)
 
-    
-
-def startApp(): # Función para iniciar la aplicación
+def startApp() -> None:
     subprocess.call('python3 productpage_monolith.py 5000', shell=True)
 
 installDependencies()
 modify_title()
 pause()
 startApp()
+
+if __name__ == "__main__":
+    pass
+
 
